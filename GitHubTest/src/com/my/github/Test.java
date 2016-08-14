74b72eb56cd965cf30809160216f74a80a02047b
GitHubOfSunny <gszxw19870517@126.com>
2016-08-14 13:01:22 +0800

    update test

--- GitHubTest/src/com/my/github/Test.java
+++ GitHubTest/src/com/my/github/Test.java
@@ -1,14 +1,18 @@
 package com.my.github;
 
 public class Test {
-private String name;
+	private String name;
 
-public String getName() {
-	return name;
-}
+	public String getName() {
+		return name;
+	}
 
-public void setName(String name) {
-	this.name = name;
-}
+	public void setName(String name) {
+		this.name = name;
+	}
 
+	@Override
+	public String toString() {
+		return super.toString();
+	}
 }
