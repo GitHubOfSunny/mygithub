f5e4dbe27a84b26427d9da322a78da2dcf536b91
GitHubOfSunny <gszxw19870517@126.com>
2016-08-14 13:09:37 +0800

    Update Test

--- GitHubTest/src/com/my/github/Test.java
+++ GitHubTest/src/com/my/github/Test.java
@@ -12,6 +12,31 @@ public class Test {
 	}
 
 	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Test other = (Test) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
+	@Override
 	public String toString() {
 		return super.toString();
 	}
